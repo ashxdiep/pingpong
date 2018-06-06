a15c0e886c746b706a6301bddd1eb807e50bba58
ash.d.321@gmail.com <ash.d.321@gmail.com>
2018-06-06 17:35:52 -0500

    moving ball

--- pong.js
+++ pong.js
@@ -32,12 +32,16 @@ class Ball extends Rect {
 const canvas = document.getElementById('pong');
 const context = canvas.getContext('2d');
 
+
 const ball = new Ball;
+
 ball.pos.x= 100;
 ball.pos.y = 50;
 
 ball.vel.x = 100;
 ball.vel.y = 100;
+
+console.log(ball);
 
 //how much time has elapsed from the last animation frame
 let lastTime;
@@ -56,10 +60,18 @@ function update(dt){
   ball.pos.x += ball.vel.x * dt;
   ball.pos.y += ball.vel.y * dt;
 
-  context.fillStyle = '#000';
+  if (ball.pos.x < 0 || ball.pos.x > canvas.width){
+    ball.vel.x = -ball.vel.x;
+  }
+
+  if (ball.pos.y < 0 || ball.pos.y > canvas.height){
+    ball.vel.y = -ball.vel.y;
+  }
+
+  context.fillStyle = 'black';
   context.fillRect(0 , 0, canvas.width, canvas.height);
 
-  context.fillStyle = '#fff';
+  context.fillStyle = 'white';
   context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y);
 }
 
